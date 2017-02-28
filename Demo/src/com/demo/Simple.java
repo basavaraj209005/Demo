a63eaf15fc5e6b44b3fbe71521bfd480c6118546
basu <basu@basu-PC>
2017-02-28 11:21:32 +0530

    final change

--- Demo/src/com/demo/Simple.java
+++ Demo/src/com/demo/Simple.java
@@ -4,13 +4,6 @@ public class Simple {
 
 	public static void main(String[] args) {
 		System.out.println("Hello");
-		System.out.println("hi");
-		System.out.println("how r u");
-		System.out.println("one");
-		System.out.println("second");
-		System.out.println("shhhhh");
-		System.out.println("sed");
-		System.out.println("jjjjj");
 		
 		
 		
